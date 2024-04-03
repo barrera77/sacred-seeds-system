647c53bd4b4571a670e05f4162525dd51697f86a
Manuel Alva <barrera_ml@hotmail.com>
2024-04-03 02:32:50 -0600

    Initial Commit

---
+++ SacredSeedsSystem/Client.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+
+
+
+namespace SacredSeedsSystem
+{
+    public class Client
+    {
+      
+        public string? ClientId { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? ContactName { get; set; } 
+        public string? PhoneNumber { get; set; }
+        public DateTime AppointmentDate {get; set; }
+
+        public Client(string clientId, string name, string address, string contactName, string phoneNumber, DateTime appointmentDate)
+        {
+            ClientId = clientId;
+            Name = name;
+            Address = address;
+            ContactName = contactName;
+            PhoneNumber = phoneNumber;
+            AppointmentDate = appointmentDate;
+        }    
+        
+        public override string ToString()
+        {
+
+            return $"{ClientId},{Name}, {Address},{ContactName},{PhoneNumber}, {AppointmentDate.ToString("MMM dd yyyy")}";
+        }
+
+        public static Client Parse(String text)
+        {
+            string[] parts = text.Split(",");
+            Client NewClient = null;
+
+            if (parts.Length == 6)
+            {
+                NewClient = new Client(parts[0], parts[1], parts[2], parts[3], parts[4], DateTime.Parse(parts[5]));
+            }
+
+            return NewClient;
+        }
+    }
+}
---
+++ SacredSeedsWebApp/Components/Pages/ClientPayment.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using SacredSeedsSystem;
+
+namespace SacredSeedsWebApp.Components.Pages
+{
+    public partial class ClientPayment
+    {
+        private List<Client>? Clients { get; set; }
+
+        public Client Client { get; set; }
+
+        [Inject]
+        public IWebHostEnvironment WebHostEnvironment { get; set; } = default!;
+
+        protected override Task OnInitializedAsync()
+        {
+            ReadFile();
+
+            return base.OnInitializedAsync();
+        }
+
+        public void ReadFile()
+        {
+            Clients = new();
+
+            string csvFilePath = $@"{WebHostEnvironment.ContentRootPath}\Data\ClientsList.csv";
+
+            using (StreamReader reader = new StreamReader(csvFilePath))
+            {
+                try
+                {
+                    if (reader.Peek() == -1)
+                    {
+                        throw new IOException("There are no records in the file to display");
+                    }
+                    //reader.ReadLine();
+                    string? currentLine;
+
+                    while ((currentLine = reader.ReadLine()) != null)
+                    {
+                        try
+                        {
+                            Clients.Add(Client.Parse(currentLine));
+                        }
+                        catch (FormatException e)
+                        {
+                            
+                        }
+                    }
+                }
+                catch (IOException e)
+                {
+                    
+                }
+                reader.Close();
+            }
+        }
+
+     
+    }
+}
