bd65339beead1ffab53991a1dde9e8f8cca42a2b
Manuel Alva <barrera_ml@hotmail.com>
2024-04-08 23:53:02 -0600

    Created Payment Section

---
+++ SacredSeedsWebApp/Components/Pages/Screen1.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace SacredSeedsWebApp.Components.Pages
+{
+    public partial class Screen1
+    {
+
+        private string clientID { get; set; } = "01-0003";
+        private string clientType { get; set; } = "investor";
+        private string firstName { get; set; } = "Leonardo";
+        private string lastName { get; set; } = "da Vinci";
+        private string clientEmail { get; set; } = "vinci.leonardo@example.com";
+        private string phoneNumber { get; set; } = "(780) 555-2467";
+        private string contactPerson { get; set; } = "Michael Johnson";
+        private string contactNumber { get; set; } = "(345) 678-9012";
+        private string appointmentCount { get; set; } = "0";
+        private string appointmentDate { get; set; } = "2024-05-05";
+        private string address { get; set; } = "2467 Kingsway Avenue NW";
+        private string city { get; set; } = "Edmonton";
+        private string province { get; set; } = "AB";
+        private string postalCode { get; set; } = "T6L 1J5";
+        private string notes { get; set; } = "Previous injury on left ankle be gentle";
+
+
+
+        
+    }
+}
